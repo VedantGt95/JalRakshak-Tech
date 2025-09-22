bd0af9cef6a85637e34d8b3fd6da3889a9355926
Arnav242005 <jadhavarnav58@gmail.com>
2025-09-22 23:56:22 +0530

    added more API

--- src/API/api.js
+++ src/API/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8080";
 
-export const registerUser = (user) => axios.post(`${BASE_URL}/api/auth/register`, user);
+export const registerUser = (user) => axios.post(`${BASE_URL}/auth/user/register`, user);
 
-export const loginUser = (user) => axios.post(`${BASE_URL}/api/auth/login`, user);
+export const loginUser = (user) => axios.post(`${BASE_URL}/auth/user/login`, user);
+
+export const loginAdmin = (admin) => axios.post(`${BASE_URL}/auth/admin/login`,admin);
