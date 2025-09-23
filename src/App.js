f15892c6d9af927090308000ad3858ecc606f435
Arnav242005 <jadhavarnav58@gmail.com>
2025-09-23 14:05:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import MapComponent from "./components/MapComponent";
+
 import AdminDashboard from "./components/AdminDashboard";
 import AdminLogin from "./components/AdminLogin";
+import UserDashboard from "./components/UserDashboard"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/map" element={<MapComponent />} />
+        <Route path="/map" element={<UserDashboard />} />
         <Route path="*" element={<Login />} />
         <Route path="/admin" element={<AdminLogin />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
