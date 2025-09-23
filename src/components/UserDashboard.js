d32d331e6cd648bccc01d3703a51eb8894c3da7b
VedantGt GitHub <vedantgharat95@gmail.com>
2025-09-23 13:55:39 +0530

    Updated role

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -2,10 +2,14 @@ import React from 'react';
 import MapComponent from './MapComponent';
 
 function UserDashboard() {
+   const user = JSON.parse(localStorage.getItem("user")) || {};
   return (
     <div>
       <h2>User Dashboard</h2>
-      <MapComponent role="USER" />
+      <MapComponent  
+       role={user.role || "USER"} 
+        userId={user.id} 
+        username={user.username} />
     </div>
   );
 }
